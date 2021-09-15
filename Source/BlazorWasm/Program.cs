a7a52edfb70002e80098180bf96fa8345123d258
McNerdius <924403+McNerdius@users.noreply.github.com>
2021-09-15 02:40:05 -0600

    Update to RC1 (#51)
    * Update to RC1
    Fixes #50
    
    * woops dropped the .32

--- Source/BlazorWasm/Program.cs
+++ Source/BlazorWasm/Program.cs
@@ -1,30 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 using TailBlazor.Core;
+using TailBlazor.Client;
 
-namespace TailBlazor.Client
-{
-    public class Program
-    {
-        public static async Task Main( string[] args )
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault( args );
-            builder.RootComponents.Add<App>( "#app" );
+var builder = WebAssemblyHostBuilder.CreateDefault( args );
+builder.RootComponents.Add<App>( "#app" );
 
-            var baseAddress = builder.Configuration["FunctionsAddress"] ?? builder.HostEnvironment.BaseAddress;
-            builder.Services.AddScoped( _ => new HttpClient { BaseAddress = new Uri( baseAddress ) } );
-            builder.Services.AddScoped<IPersonService, HttpPersonService>();
+var baseAddress = builder.Configuration["FunctionsAddress"] ?? builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped( _ => new HttpClient { BaseAddress = new Uri( baseAddress ) } );
+builder.Services.AddScoped<IPersonService, HttpPersonService>();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
