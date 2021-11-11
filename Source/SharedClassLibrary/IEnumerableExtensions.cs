fc782fc5367a3c28318281509dd8a9aab1589827
McNerdius <mcnerd@techie.com>
2021-11-11 10:16:16 -0700

    remove redundant/implicit usings

--- Source/SharedClassLibrary/IEnumerableExtensions.cs
+++ Source/SharedClassLibrary/IEnumerableExtensions.cs
@@ -1,19 +1,13 @@
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 #pragma warning disable CA1050 // yes i am evil and putting an extension method outside a namespace
 public static class IEnumerableExtensions
 {
-    private static Random random = new Random();
-
     public static IEnumerable<T> RandomElements<T>( this IEnumerable<T> source, int elements )
     {
         var repeat = (int) Math.Ceiling( elements / (double) source.Count() );
 
         return Enumerable.Range( 0, repeat ).SelectMany( _ => source )
-                                            .OrderBy( _ => random.Next() )
+                                            .OrderBy( _ => Random.Shared.Next() )
                                             .Take( elements );
     }
 }
--- Source/SharedClassLibrary/IPersonService.cs
+++ Source/SharedClassLibrary/IPersonService.cs
@@ -1,6 +1,4 @@
 
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace TailBlazor.Core
 {
