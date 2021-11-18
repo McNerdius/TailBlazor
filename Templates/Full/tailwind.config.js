ae1abb7dbf0210dfad090f22c45936565b4bda02
McNerdius <mcnerd@techie.com>
2021-11-18 10:18:27 -0700

    dupe into Full; remove Functions API

---
+++ Templates/Full/tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        './{BlazorWasm,BlazorServer,RazorClassLibrary}/**/*.{razor,html,cshtml}'
+    ],
+    // mode: 'jit',
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.blue,
+                secondary: colors.amber,
+                accent: "var(--accent-color)",
+            },
+            keyframes: {
+                hscroll: {
+                    '0%': { transform: 'translateX(10vw)' },
+                    '50%': { transform: 'translateX(calc(90vw - 100%))' },
+                    '100%': { transform: 'translateX(10vw)' },
+                }
+            },
+            animation: {
+                hscroll: 'hscroll 300s infinite linear'
+            },
+            screens: {
+                'no-hover': { 'raw': '(hover:none)' }
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/line-clamp')
+    ],
+}
