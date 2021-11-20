e9373b89d1bad90916118e793750ee63eeb46601
McNerdius <mcnerd@techie.com>
2021-11-19 18:19:16 -0700

    better naming

--- Templates/Full/BlazorServer/Pages/Error.cshtml.cs
+++ Templates/Full/BlazorServer/Pages/Error.cshtml.cs
@@ -3,7 +3,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Server.Pages
+namespace BlazorServer.Pages
 {
     [ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
     [IgnoreAntiforgeryToken]
