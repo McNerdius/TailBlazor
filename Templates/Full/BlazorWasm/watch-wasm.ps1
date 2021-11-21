e2301bc6cdb4a52058adea1628599c420e0cba68
McNerdius <mcnerd@techie.com>
2021-11-21 09:35:22 -0700

    wait for RCL to build before Tailwind watch

--- Templates/Full/BlazorWasm/watch-wasm.ps1
+++ Templates/Full/BlazorWasm/watch-wasm.ps1
@@ -1,4 +1,6 @@
 # dotnet build -- -property TailwindBuild=false  # [wonky things](https://github.com/dotnet/aspnetcore/issues/34500) can happen without explicit pre-build - fixed in RC1 (?)
-start "dotnet" -ArgumentList "watch --project ../FunctionsAPI msbuild /t:RunFunctions" # -NoNewWindow
 start "dotnet" -ArgumentList "watch run -- -property TailwindBuild=false" # -NoNewWindow
+
+while (!(Test-Path "../RazorClassLibrary/obj/scopedcss/bundle/RazorClassLibrary.styles.css")) { sleep -ms 200 } # tailwind needs this file
+
 npm --prefix ../ run watch-wasm
