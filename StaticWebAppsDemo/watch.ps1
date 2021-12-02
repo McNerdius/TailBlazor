4289d91f5fc0e3a9a50b9ab9d078932041ea0000
McNerdius <mcnerd@techie.com>
2021-12-01 18:36:44 -0700

    reduce full rebuilds when using CLI/VSC watch Fixes #86

--- StaticWebAppsDemo/watch.ps1
+++ StaticWebAppsDemo/watch.ps1
@@ -1,7 +1,7 @@
 start "dotnet" -ArgumentList "watch --project ./FunctionsAPI msbuild /t:RunFunctions" # -NoNewWindow
 sleep 1
 cd "BlazorWasm"
-start "dotnet" -ArgumentList "watch -- run -p:TailwindBuild=false" # -NoNewWindow
+start "dotnet" -ArgumentList "watch" # -NoNewWindow
 
 while (!(Test-Path "./obj/scopedcss/bundle/BlazorWasm.styles.css")) { sleep -ms 200 } # tailwind needs this file
 
--- Templates/Full/BlazorWasm/watch-wasm.ps1
+++ Templates/Full/BlazorWasm/watch-wasm.ps1
@@ -1,4 +1,4 @@
-start "dotnet" -ArgumentList "watch -- run -p:TailwindBuild=false" # -NoNewWindow
+start "dotnet" -ArgumentList "watch" # -NoNewWindow
 
 while (!(Test-Path "../RazorClassLibrary/obj/scopedcss/bundle/RazorClassLibrary.styles.css")) { sleep -ms 200 } # tailwind needs this file
 
