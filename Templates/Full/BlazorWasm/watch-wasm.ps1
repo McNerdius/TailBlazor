f9a251ea93db232572b8c1e27ec62125829ff117
McNerdius <mcnerd@techie.com>
2021-12-03 04:13:21 -0700

    first run: CSS builds prior to npm install, must wait

--- Templates/Full/BlazorWasm/watch-wasm.ps1
+++ Templates/Full/BlazorWasm/watch-wasm.ps1
@@ -1,5 +1,6 @@
 start "dotnet" -ArgumentList "watch" # -NoNewWindow
 
-while (!(Test-Path "../RazorClassLibrary/obj/scopedcss/bundle/RazorClassLibrary.styles.css")) { sleep -ms 200 } # tailwind needs this file
+while (!(Test-Path "../RazorClassLibrary/obj/scopedcss/bundle/RazorClassLibrary.styles.css")) { sleep -ms 100 } # tailwind needs this file
+while (!(Test-Path "../node_modules/.install-stamp")) { sleep -ms 100 } # npm run needs this file 
 
-npm --prefix ../ run watch-wasm
+npm --prefix ../ run watch
--- Templates/Minimal/TailBlazorServer/watch.ps1
+++ Templates/Minimal/TailBlazorServer/watch.ps1
@@ -1,5 +1,6 @@
 start "dotnet" -ArgumentList "watch" # -nonewwindow
 
-while (!(Test-Path "./obj/scopedcss/bundle/TailBlazorServer.styles.css")) { sleep -ms 200 } # tailwind needs this file
+while (!(Test-Path "./obj/scopedcss/bundle/TailBlazorServer.styles.css")) { sleep -ms 100 } # tailwind needs this file
+while (!(Test-Path "./node_modules/.install-stamp")) { sleep -ms 100 } # npm run needs this file 
 
 npm run watch
