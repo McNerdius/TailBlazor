87064733612ac76635e22239bdccfa52079372cf
McNerdius <mcnerd@techie.com>
2021-12-02 20:00:19 -0700

    initial Tailwind makeover

---
+++ Templates/Minimal/TailBlazorPages/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["**/*.{razor,cshtml}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            flex: {
+                'fill': '1 0 100%',
+            },
+        }
+    },
+    plugins: [],
+}
