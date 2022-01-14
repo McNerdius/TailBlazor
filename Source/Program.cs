acab0888ea309ac2447e6ddb26e14f3c0218daeb
McNerdius <mcnerd@techie.com>
2022-01-14 16:50:20 -0700

    IMarkdownContentProvider => IContentProvider

--- Source/Program.cs
+++ Source/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<App>( "#app" );
 builder.RootComponents.Add<HeadOutlet>( "head::after" );
 
 #if RELEASE
-builder.Services.AddSingleton<IMarkdownContentProvider, EmbeddedHtmlProvider>();
+builder.Services.AddSingleton<IContentProvider, EmbeddedHtmlProvider>();
 #else
 builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
 builder.Services.AddScoped<IContentProvider, LocalMarkdownProvider>();
