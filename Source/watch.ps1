6f110d106c5f5add5ae3212e50ce2b7cff8057ab
McNerdius <924403+McNerdius@users.noreply.github.com>
2022-01-30 00:45:22 -0800

    tweak pixels (#128)

--- Source/watch.ps1
+++ Source/watch.ps1
@@ -1,4 +1,5 @@
-start "dotnet" -ArgumentList "watch" # -NoNewWindow
+# dotnet build
+start "dotnet" -ArgumentList "watch run --property:tailwindbuild=false" # -NoNewWindow
 
 while (!(Test-Path "./node_modules/.package-lock.json")) { sleep -ms 100 } 
 while (!(Test-Path "./obj/scopedcss/bundle/TailBlazor.styles.css")) { sleep -ms 200 } # tailwind needs this file
