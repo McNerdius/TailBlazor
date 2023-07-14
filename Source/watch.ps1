59ab798cd0d5d916dc52f79c701a02b989ea46f4
McNerdius <jacob@mcnerd.dev>
2023-07-13 20:20:04 -0600

    nuke some blazor files

--- Source/watch.ps1
+++
@@ -1,9 +0,0 @@
-# dotnet build
-# start "dotnet" -ArgumentList "watch run --property:tailwindbuild=false" # -NoNewWindow
-
-start "dotnet" -ArgumentList "watch" # -NoNewWindow
-
-while (!(Test-Path "./node_modules/.package-lock.json")) { sleep -ms 100 } 
-while (!(Test-Path "./obj/scopedcss/bundle/TailBlazor.styles.css")) { sleep -ms 200 } # tailwind needs this file
-
-npm run watch
