e62da02ffa919009e44a41cf6985ceb8536c768a
McNerdius <jacob@mcnerd.dev>
2023-07-13 20:30:51 -0600

    legit elements barrel

--- Blit/src/elements.ts
+++ Blit/src/elements.ts
@@ -1,1 +1,38 @@
-export * from './elements';
+import { AwesomeLoader } from "./elements/awesome-loader";  //✅
+import { DarkSwitch } from "./elements/dark-switch";        //✅
+import { IconLink } from "./elements/icon-link";            //✅
+import { IndexPage } from "./elements/index-page";          //✅
+import { MainLayout } from "./main-layout";                 //✅
+import { NavMenu } from "./elements/nav-menu";              //✅
+import { NotFound } from "./elements/not-found";            //✅
+import { StaticContent } from "./elements/static-content";  //✅
+import { TailBlazorApp } from "./tailblazor-app";           //✅
+
+export
+{
+    AwesomeLoader,  //✅
+    DarkSwitch,     //✅
+    IconLink,       //✅
+    IndexPage,      //✅
+    MainLayout,     //✅
+    NavMenu,        //✅
+    NotFound,       //✅
+    StaticContent,  //✅
+    TailBlazorApp   //✅
+};
+
+declare global
+{
+    interface HTMLElementTagNameMap
+    {
+        'awesome-loader': AwesomeLoader,    //✅
+        'dark-switch': DarkSwitch,          //✅
+        'icon-link': IconLink,              //✅
+        'index-page': IndexPage,            //✅
+        'main-layout': MainLayout,          //✅
+        'nav-menu': NavMenu,                //✅
+        'not-found': NotFound,              //✅
+        'static-content': StaticContent,    //✅
+        'tailblazor-app': TailBlazorApp     //✅
+    }
+};
