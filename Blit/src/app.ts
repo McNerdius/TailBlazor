f4c3a440da410f7ee38ec0224bbb47928e671d34
McNerdius <jacob@mcnerd.dev>
2023-07-14 10:05:46 -0600

    swap vaadin for lit-labs router

--- Blit/src/app.ts
+++ Blit/src/app.ts
@@ -1,38 +1,20 @@
-import { Routes } from "./routes";
-import { Router } from '@vaadin/router';
-import { LitElement, css, html, unsafeCSS } from 'lit';
-import { customElement, query } from 'lit/decorators.js'
-import twBaseOnly from "tailwindcss/base.css?inline";
-
-// services go here:
-// import { provide } from "@lit-labs/context";
+import { LitElement, css, html } from 'lit';
+import { customElement } from 'lit/decorators.js'
+import { Router } from "@lit-labs/router";
 
 @customElement('tailblazor-app')
 export class TailBlazorApp extends LitElement
 {
-    static styles = [
-        unsafeCSS(twBaseOnly),
-        css`
-        :host, div{
-            display: block;
-            height: 100%;
-        }`
-        // class="h-full w-full m-0 p-0"
-    ]
+    static styles = css`:host, div{ display: block; height: 100%; }`;
 
-    protected async firstUpdated()
-    {
-        this.router = new Router(this.outlet);
-        this.router.setRoutes(Routes);
-    }
+    #router = new Router(this,
+    [
+        { path: "/",      render: () => html`<static-content page="overview"></static-content>` },
+        { path: "/:page", render: ({ page }) => html`<static-content .page=${page!}></static-content>` }
+    ]);
 
-    private router!: Router;
-
-    @query("#outlet") public outlet!: HTMLDivElement;
-
-    render()
-    {
-        return html`<div id="outlet"></div>`;
-
-    }
+    render = () => html`
+        <main-layout>
+            ${this.#router.outlet()}
+        </main-layout>`;
 }
--- Blit/src/elements/static-content.ts
+++ Blit/src/elements/static-content.ts
@@ -1,27 +1,18 @@
 import { css, html } from 'lit';
 import { until } from 'lit/directives/until.js';
-import { customElement, state } from 'lit/decorators.js'
-
-import { BeforeEnterObserver, RouterLocation } from '@vaadin/router';
+import { customElement, property } from 'lit/decorators.js'
 
 import { BlitElement } from './blit-element';
-
-// import { cache } from 'lit/directives/cache.js';
 
 // import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/components/include/include.js';
 import 'https://cdn.jsdelivr.net/gh/zerodevx/zero-md@2/dist/zero-md.min.js'
 
 @customElement('static-content')
-export class StaticContent extends BlitElement implements BeforeEnterObserver
+export class StaticContent extends BlitElement
 {
     static styles = [ ...super.styles, css`` ];
 
-    @state() private page!: string;
-
-    async onBeforeEnter(location: RouterLocation)
-    {
-        this.page = location.params["static"] as string;
-    }
+    @property() page!: string;
 
     async loadContent()
     {
