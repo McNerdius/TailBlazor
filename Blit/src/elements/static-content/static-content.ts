37b2a3a027a68a186317f28d9c0a1e62db7479f7
McNerdius <jacob@mcnerd.dev>
2023-07-14 21:07:22 -0600

    fix up static-content

--- Blit/src/elements/static-content/static-content.ts
+++ Blit/src/elements/static-content/static-content.ts
@@ -1,8 +1,10 @@
-import { css, html } from 'lit';
+import { html, unsafeCSS } from 'lit';
 import { until } from 'lit/directives/until.js';
 import { customElement, property } from 'lit/decorators.js'
 
-import { BlitElement } from './blit-element';
+import { BlitElement } from '../blit-element';
+
+import staticCSS from './static-content.css?inline';
 
 // import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.1.0/dist/components/include/include.js';
 import 'https://cdn.jsdelivr.net/gh/zerodevx/zero-md@2/dist/zero-md.min.js'
@@ -10,7 +12,7 @@ import 'https://cdn.jsdelivr.net/gh/zerodevx/zero-md@2/dist/zero-md.min.js'
 @customElement('static-content')
 export class StaticContent extends BlitElement
 {
-    static styles = [ ...super.styles, css`` ];
+    static styles = [...super.styles, unsafeCSS(staticCSS) ];
 
     @property() page!: string;
 
@@ -18,7 +20,7 @@ export class StaticContent extends BlitElement
     {
         if ((await fetch(`/content/${this.page}.md`, { method: "HEAD" }).catch()).ok)
         {
-            return html`<zero-md src="/content/${this.page}.md"}></zero-md>`;
+            return html`<zero-md src="/content/${this.page}.md" no-shadow></zero-md>`;
         }
         else
         {
@@ -27,7 +29,8 @@ export class StaticContent extends BlitElement
     }
 
     render = () => html`
-        <div class="w-full h-full">
+        <div class="w-full h-full prose prose-sm md:prose-base lg:prose-lg 2xl:prose-xl dark:prose-invert
+                markdown">
             ${until(this.loadContent(), html`<awesome-loader></awesome-loader>`)}
         </div>
     `;
