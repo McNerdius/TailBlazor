ad5d898e5e25bae939df2f265c4548c064ffaf82
McNerdius <jacob@mcnerd.dev>
2023-07-24 23:03:17 -0600

    post load hash nav working

--- source/elements/static-content/static-content.ts
+++ source/elements/static-content/static-content.ts
@@ -16,33 +16,49 @@ import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 @customElement('static-content')
 export class StaticContent extends BlitElement implements BeforeEnterObserver
 {
-    static styles = [...super.styles, unsafeCSS(staticCSS), unsafeCSS(codeBlockCSS),unsafeCSS(prismCSS) ];
+    static styles = [ ...super.styles, unsafeCSS(staticCSS), unsafeCSS(codeBlockCSS), unsafeCSS(prismCSS) ];
 
     @state() private page!: string;
-
+    
     async onBeforeEnter(location: RouterLocation)
     {
         this.page = location.params["static"] as string;
     }
 
-    async loadContent()
-    {
-        let foo;
-
-        if ((foo = await fetch(`/content/${this.page}.html`).catch()).ok)
-        {
-            return unsafeHTML(await foo.text());
-        }
-        else
-        {
-            return html`<not-found></not-found>`;
-        }
+    connectedCallback() {
+        super.connectedCallback();
+        window.addEventListener('hashchange', () => this.scrollToHash());
     }
 
-    render = () => html`
+    scrollToHash()
+    {
+        console.log("scroll");
+        const anchor = this.renderRoot.querySelector(`${location.hash}`);
+        console.dir(anchor);
+        anchor?.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    
+
+    async loadContent()
+    {
+        console.log(`@load: (${location.hash})`);
+
+        let content = await fetch(`/content/${this.page}.html`).then(r => r.text()).catch();
+    
+        return content
+            ? html`<div class="content">${unsafeHTML(content)}</div>`
+            : html`<not-found></not-found>`;
+    }
+
+    render()
+    {
+        console.log(`@render: (${location.hash})`);
+
+        return html`
         <div class="w-full h-full prose prose-sm md:prose-base lg:prose-lg 2xl:prose-xl dark:prose-invert
                 markdown">
             ${until(this.loadContent(), html`<awesome-loader></awesome-loader>`)}
-        </div>
-    `;
+        </div>`;
+    }
 }
