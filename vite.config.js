06dc07e10167c38b87254612094b7e880eae46f9
McNerdius <jacob@mcnerd.dev>
2023-08-16 01:17:19 -0600

    tweak the vite config

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import { defineConfig } from 'vite';
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: { externalLiveBindings: false },
+        }
+    },
+    esbuild: {
+        drop: ["console"],
+        legalComments: "external",
+    },
     plugins: [
         visualizer({ filename: "dist/bundle_stats.html", title: "Bundle Stats", gzipSize: true, brotliSize: true })
     ],
-    esbuild: {
-        drop: [ "console" ],
-
-    }
-    // build: {
-        
-    //     rollupOptions: {
-    //     }
-    // }
 });
