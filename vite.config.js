1c9f1cfbfed7e49f599adc1a49ac564c71b3a4af
McNerdius <jacob@mcnerd.dev>
2023-10-13 18:06:35 -0600

    prod things for prod only

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,25 @@ console.log("vite config loaded");
 import { defineConfig } from 'vite';
 import { visualizer } from "rollup-plugin-visualizer";
 
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            output: { externalLiveBindings: false },
+// @ts-ignore unused parameters
+export default defineConfig(({ command, mode, ssrBuild }) => 
+{
+    if (command === 'build')
+    {
+        return {
+            build: {
+                rollupOptions: {
+                    output: { externalLiveBindings: false },
+                }
+            },
+            esbuild: {
+                drop: ["console"],
+                legalComments: "external",
+            },
+            plugins: [
+                visualizer({ filename: "dist/bundle_stats.html", title: "Bundle Stats", gzipSize: true, brotliSize: true })
+            ],
         }
-    },
-    esbuild: {
-        drop: ["console"],
-        legalComments: "external",
-    },
-    plugins: [
-        visualizer({ filename: "dist/bundle_stats.html", title: "Bundle Stats", gzipSize: true, brotliSize: true })
-    ],
+    }
+    else return {}
 });
